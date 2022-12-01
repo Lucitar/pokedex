6d9bd40647cf9ad9c3fe713f5dcde6e184dd37fd
Victor Lucio (Lucitar) <victorlu3391@gmail.com>
2022-11-30 23:45:43 -0300

    Add Paginação Funcional

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Pokedex from "./components/Pokedex";
 import { getPokemonData, getPokemons } from "./api";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
 
+  const itensPerPage = 25;
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons();
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
@@ -20,6 +23,7 @@ function App() {
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
     } catch (error) {
       console.log("fetchPokemonsError: ", error);
     }
@@ -27,12 +31,18 @@ function App() {
 
   useEffect(() => {
     fetchPokemons();
-  }, []);
+  }, [page]); // Vai renderizar de novo quando o valor do page mudar
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading} />
+      <Pokedex
+        pokemons={pokemons}
+        loading={loading}
+        page={page}
+        setPage={setPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 }
